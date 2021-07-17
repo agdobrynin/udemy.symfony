6fec9c9dc3fd7bdab9a6929a862179ca3c7bd902
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-07-17 13:17:21 +0400

    Make new fixtures for User and BlogPost

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,16 +3,26 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private const REFERENCE_TEST_USER_1 = 'test.user.1';
+    private const REFERENCE_TEST_USER_2 = 'test.user.2';
+
     public function load(ObjectManager $manager)
+    {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
     {
         $blogPost = new BlogPost();
         $blogPost->setTitle('Yess');
-        $blogPost->setAuthor('Iva Iva');
+        $blogPost->setAuthor($this->getReference(self::REFERENCE_TEST_USER_1));
         $blogPost->setContent('Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo.');
         $blogPost->setCreatedAt(new \DateTime());
         $blogPost->setSlug('lo-lo');
@@ -20,11 +30,40 @@ class AppFixtures extends Fixture
 
         $blogPost = new BlogPost();
         $blogPost->setTitle('Post forman');
-        $blogPost->setAuthor('Popila');
+        $blogPost->setAuthor($this->getReference(self::REFERENCE_TEST_USER_2));
         $blogPost->setContent('Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
         $blogPost->setCreatedAt(new \DateTime());
         $blogPost->setSlug('ko-ko');
         $manager->persist($blogPost);
+
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user1 = (new User())
+            ->setLogin('alex.d')
+            ->setName('Alex D.')
+            ->setEmail('alex.d@gmail.com')
+            ->setPassword('qwerty#2');
+
+        $this->addReference(self::REFERENCE_TEST_USER_1, $user1);
+        $manager->persist($user1);
+
+        $user2 = (new User())
+            ->setLogin('klim.v')
+            ->setName('Klim Voroshilov')
+            ->setEmail('klim@mail.ru')
+            ->setPassword('2#qwerty');
+
+        $this->addReference(self::REFERENCE_TEST_USER_2, $user2);
+        $manager->persist($user2);
 
         $manager->flush();
     }
