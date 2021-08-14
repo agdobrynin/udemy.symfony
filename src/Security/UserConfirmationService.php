cfcbf644a180bce82777f5788717cef4aaa59b2e
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-08-14 10:13:24 +0400

    Custom logging channel

--- src/Security/UserConfirmationService.php
+++ src/Security/UserConfirmationService.php
@@ -7,27 +7,31 @@ namespace App\Security;
 use App\Exception\InvalidConfirmationTokenException;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Psr\Log\LoggerInterface;
 
 final class UserConfirmationService
 {
     private $userRepository;
     private $entityManager;
+    private $logger;
 
-    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager)
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager, LoggerInterface $logger)
     {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
     public function confirmUser(string $confirmToken)
     {
         if ($user = $this->userRepository->findOneByConfirmToken($confirmToken)) {
+            $this->logger->debug(sprintf('User found "%s" with login "%s" and email "%s"', $user->getName(), $user->getLogin(), $user->getEmail()));
             $user->setIsActive(true);
             $user->setConfirmationToken(null);
             $this->entityManager->flush();
             return;
         }
 
+        $this->logger->debug(sprintf('User not found by token "%s"', $confirmToken));
         throw new InvalidConfirmationTokenException('Confirmation token is invalid.');
     }
 }
