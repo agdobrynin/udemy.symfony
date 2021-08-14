1b37afa654c01938a88e72c5ff1b3fedc188ab1b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-08-14 14:35:51 +0400

    Custom operation in UserCrudController - hashing password

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,23 +3,42 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    public function persistEntity(EntityManagerInterface $entityManager, $entity): void
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $this->encodeUserPassword($entity);
+        parent::persistEntity($entityManager, $entity);
     }
-    */
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entity): void
+    {
+        $this->encodeUserPassword($entity);
+        parent::updateEntity($entityManager, $entity);
+    }
+
+    /**
+     * @param User $entity
+     */
+    private function encodeUserPassword(User $user): void
+    {
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $user->getPassword()));
+    }
 }
