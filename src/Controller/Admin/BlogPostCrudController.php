201580ce567f97a3bd1c86d1db58965872ca2357
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-08-28 11:01:35 +0400

    EasyAdmin: use actions as inline in BlogPost entity

--- src/Controller/Admin/BlogPostCrudController.php
+++ src/Controller/Admin/BlogPostCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\BlogPost;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -16,6 +19,16 @@ class BlogPostCrudController extends AbstractCrudController
         return BlogPost::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return parent::configureCrud($crud)->showEntityActionsInlined();
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return parent::configureActions($actions)->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -25,7 +38,7 @@ class BlogPostCrudController extends AbstractCrudController
             TextEditorField::new('content'),
             AssociationField::new('author'),
             AssociationField::new('mediaObjects'),
-            AssociationField::new('comments')->setColumns(12),
+            AssociationField::new('comments'),
         ];
     }
 }
